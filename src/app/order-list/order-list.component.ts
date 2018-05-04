6dc245ddd9aad11d76a4a4698d3e594d51d0fe19
ezacca <ezacca@silentiumapps.com>
2018-05-04 15:03:20 -0300

    data binding 2.0

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../../interfaces/order.interface';
 
 @Component({
   selector: 'app-order-list',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
-
+  public orders:Array<IOrder>;
   constructor() { }
 
   ngOnInit() {
+    this.orders = [{
+      hungryPerson:"FX",
+      main:"Napolitana",
+      salads:['Brocoli','Lentejas','Chauchas']
+    },
+    {
+      hungryPerson:"Pancho",
+      main:"Pollo",
+      salads:['Huevo','Coreanito','Zapallito']
+    },
+    {
+      hungryPerson:"Ezequiel",
+      main:"Suprema",
+      salads:['Arroz','Lentejas','Fideos']
+    }]
   }
 
 }
--- src/interfaces/order.interface.ts
+++ src/interfaces/order.interface.ts
@@ -1,4 +1,5 @@
 export interface IOrder {
     main: string;
     salads: Array<string>;
+    hungryPerson:string;
 }
